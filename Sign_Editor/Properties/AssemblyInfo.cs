e6fce916820b39960ba4f1cf3e4f256379c94f69
Enerdy <rodrigo.rente@live.com>
2014-07-16 17:32:02 +0100

    Fix AssemblyInfo

--- Sign_Editor/Properties/AssemblyInfo.cs
+++ Sign_Editor/Properties/AssemblyInfo.cs
@@ -22,11 +22,11 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6a36cde4-6b2b-4d8e-a66b-86398902674b")]
 
-// Version information for an assembly consists of the following three values:
+// Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
+//		Major Version
+//		Minor Version 
+//		Build Number
 //		DDMM at the time of build
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
