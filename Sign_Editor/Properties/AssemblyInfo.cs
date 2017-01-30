6f2d63a33c9ed9a7ac2613e10f77940e13362b12
Enerdy <rodrigo.rente@live.com>
2017-01-30 16:07:28 +0000

    API 2.0 - TShock Mintaka

--- Sign_Editor/Properties/AssemblyInfo.cs
+++ Sign_Editor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("Enerdy")]
 [assembly: AssemblyProduct("Sign_Editor")]
-[assembly: AssemblyCopyright("Copyright © Enerdy 2014-2016")]
+[assembly: AssemblyCopyright("Copyright © Enerdy 2014-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.2")]
-[assembly: AssemblyFileVersion("1.2.2")]
+[assembly: AssemblyVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.3")]
